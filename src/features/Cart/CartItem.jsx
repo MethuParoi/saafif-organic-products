df37ce123a347b0560423e97be1da9797b822281
MethuParoi <paroimethu@gmail.com>
2024-07-12 23:41:05 +0530

    multiple image view functionality added to the cart

--- src/features/Cart/CartItem.jsx
+++ src/features/Cart/CartItem.jsx
@@ -6,6 +6,7 @@ import {
   getCurrentQuantityById,
   getTotalCartQunatity,
 } from "./cartSlice";
+import { useState } from "react";
 // import { useEffect } from "react";
 
 function CartItem({ item }) {
@@ -19,33 +20,69 @@ function CartItem({ item }) {
     description,
   } = item;
 
+  const image = img.split(" ");
+
+  const image1 = image[0];
+  const image2 = image[1];
+  const [displayImage, setDisplayImage] = useState(image1);
+
+  function handleDisplayImage(imageKey) {
+    if (imageKey === "image1") {
+      setDisplayImage(image1);
+    }
+    if (imageKey === "image2") {
+      setDisplayImage(image2);
+    }
+  }
+
   const totalQantity = useSelector(getTotalCartQunatity);
   const currentQuantity = useSelector(getCurrentQuantityById(productId));
 
-
-
   return (
     <li className="py-4 flex flex-col md:relative">
-      <div className="flex lg:flex-row flex-col gap-8">
-        <div className="flex justify-center items-center md:flex-initial md:justify-start md:items-start">
-          <img
-            className="rounded-lg md:h-[14rem] h-[14rem] w-[14rem] md:w-[12rem] object-contain object-center border-2 border-gray-400 shadow-lg"
-            src={img}
-            alt=""
-          />
+      {/* photo and description */}
+      <div className="flex flex-col lg:gap-y-5">
+        <div className="flex lg:flex-row flex-col gap-8">
+          <div className="flex justify-center items-center md:flex-initial md:justify-start md:items-start">
+            <img
+              className="rounded-lg md:h-[14rem] h-[14rem] w-[14rem] md:w-[12rem] object-contain object-center border-2 border-gray-400 shadow-lg"
+              src={displayImage}
+              alt=""
+            />
+          </div>
+          <div>
+            <p className="pb-2 text-xl font-semibold">
+              {quantity}&times; {title}
+            </p>
+
+            <p className="text-md text-gray-500 font-semibold">
+              ৳ {unitPrice * currentQuantity}
+            </p>
+
+            <p className="pt-2 pr-5 text-sm text-gray-500 max-w-[50rem]">
+              {description}
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="pb-2 text-xl font-semibold">
-            {quantity}&times; {title}
-          </p>
 
-          <p className="text-md text-gray-500 font-semibold">
-            ৳ {unitPrice * currentQuantity}
-          </p>
-
-          <p className="pt-2 pr-5 text-sm text-gray-500 max-w-[50rem]">
-            {description}
-          </p>
+        {/* photo options */}
+        <div className="self-start pl-[3.5rem]">
+          <button className=" ">
+            <img
+              className="rounded-lg md:h-[2rem] h-[14rem] w-[14rem] md:w-[2rem] object-contain object-center border-2 border-gray-400 hover:border-primary hover:border-4 shadow-lg"
+              src={image1}
+              alt=""
+              onClick={() => handleDisplayImage("image1")}
+            />
+          </button>
+          <button className="pl-[1rem]">
+            <img
+              className="rounded-lg md:h-[2rem] h-[14rem] w-[14rem] md:w-[2rem] object-contain object-center border-2 border-gray-400 hover:border-primary hover:border-4 shadow-lg"
+              src={image2}
+              alt=""
+              onClick={() => handleDisplayImage("image2")}
+            />
+          </button>
         </div>
       </div>
 
