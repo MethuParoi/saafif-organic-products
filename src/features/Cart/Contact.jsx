d125c96f1c6ccdf044c24c5f2e9483fe83396bde
Muhit Khan <muhitkhan_bd@outlook.com>
2024-07-29 15:37:08 +0600

    Customer Details Form Updated

--- src/features/Cart/Contact.jsx
+++ src/features/Cart/Contact.jsx
@@ -20,6 +20,7 @@ export default function Contact({ cartData }) {
         const data = await response.json();
 
         if (data.success) {
+            toast.remove();
             toast.success("Order Placed!");
             event.target.reset();
         } else {
@@ -29,73 +30,100 @@ export default function Contact({ cartData }) {
     };
 
     return (
-        <div className="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md">
+        <div className="flex flex-col justify-center my-10 py-6 ">
             <form onSubmit={onSubmit} className="space-y-6">
-                <h2 className="text-2xl font-bold text-center text-gray-800">Order Details</h2>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    required
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="tel"
-                    name="phone"
-                    placeholder="Phone Number"
-                    required
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email (optional)"
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="text"
-                    name="house"
-                    placeholder="House"
-                    required
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="text"
-                    name="area"
-                    placeholder="Area"
-                    required
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="text"
-                    name="police_station"
-                    placeholder="Police Station"
-                    required
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="text"
-                    name="district"
-                    placeholder="District"
-                    required
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <input
-                    type="text"
-                    name="zip_code"
-                    placeholder="Zip Code (optional)"
-                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
+                <h2 className="text-2xl font-bold text-center text-primary mb-4">Contact Details</h2>
 
-                <button
-                    type="submit"
-                    className="w-full p-3 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                    Place Order
-                </button>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div>
+                        <label htmlFor="name" className="block text-lg font-medium text-gray-850">Full Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Full Name"
+                            required
+                            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="phone" className="block text-lg font-medium text-gray-850">Phone Number</label>
+                        <input
+                            type="tel"
+                            name="phone"
+                            placeholder="Phone Number"
+                            required
+                            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                        />
+                    </div>
+
+                    <div className="md:col-span-2">
+                        <label htmlFor="email" className="block text-lg font-medium text-gray-850">Email (optional)</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email (optional)"
+                            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                        />
+                    </div>
+
+                    <div className="md:col-span-2">
+                        <label htmlFor="address" className="block text-lg font-medium text-gray-850">Address</label>
+                        <textarea
+                            name="address"
+                            placeholder="Street Address, House No."
+                            required
+                            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="city" className="block text-lg font-medium text-gray-850">City</label>
+                        <input
+                            type="text"
+                            name="city"
+                            placeholder="City"
+                            required
+                            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="district" className="block text-lg font-medium text-gray-850">District</label>
+                        <input
+                            type="text"
+                            name="district"
+                            placeholder="District"
+                            required
+                            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                        />
+                    </div>
+
+                    {/* <div>
+                        <label htmlFor="postal_code" className="block text-lg font-medium text-gray-850">Postal Code</label>
+                        <input
+                            type="text"
+                            name="postal_code"
+                            placeholder="Postal Code"
+                            required
+                            className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                        />
+                    </div> */}
+                </div>
+
+                <div className="text-center mt-6">
+                    <button
+                        type="submit"
+                        className="text-white text-md font-semibold bg-primary hover:bg-primaryHover px-4 py-3 rounded-3xl"
+                    >
+                        Place Order
+                    </button>
+                </div>
             </form>
+
+
             <span className="block mt-4 text-center text-gray-700">{result}</span>
-        </div>
+        </div >
     );
 }
 
@@ -104,47 +132,3 @@ export default function Contact({ cartData }) {
 
 
 
-
-// import React from "react";
-
-// export default function Contact({ cartData }) {
-//     const [result, setResult] = React.useState("");
-
-//     const onSubmit = async (event) => {
-//         event.preventDefault();
-//         setResult("Sending....");
-//         const formData = new FormData(event.target);
-
-//         formData.append("access_key", process.env.WEB3_FORMS_ACCESS_KEY);
-//         formData.append("cart_data", JSON.stringify(cartData));
-
-//         const response = await fetch("https://api.web3forms.com/submit", {
-//             method: "POST",
-//             body: formData,
-//         });
-
-//         const data = await response.json();
-
-//         if (data.success) {
-//             setResult("Order Submitted Successfully");
-//             event.target.reset();
-//         } else {
-//             console.log("Error", data);
-//             setResult(data.message);
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <form onSubmit={onSubmit}>
-//                 <input type="text" name="name" placeholder="Name" required />
-//                 <input type="tel" name="phone" placeholder="Phone Number" required />
-//                 <input type="email" name="email" placeholder="Email (optional)" />
-//                 <textarea name="address" placeholder="Address (House, Area, Police Station, District, Zip Code (optional))" required></textarea>
-
-//                 <button type="submit">Place Order</button>
-//             </form>
-//             <span>{result}</span>
-//         </div>
-//     );
-// }
