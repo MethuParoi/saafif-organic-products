3d85d0c0e86cc6a128f5260ca51cc1ebc5c94610
MethuParoi <paroimethu@gmail.com>
2024-07-30 18:52:14 +0530

    .env file added to the project

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,22 +1,8 @@
-// import { createClient } from "@supabase/supabase-js";
-// // Load environment variables
-// // import dotenv from "dotenv";
-// // dotenv.config();
-
-// // const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_URL || "";
-// const SUPABASE_KEY =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNub3l1aHVtenhtcmJkemVhY3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4Mzg0NzksImV4cCI6MjAzNTQxNDQ3OX0.koHX73Dj_D8N5Z11iF-TUw6okVTZ0jGbMrppAECXfTY";
-// const supabaseUrl = "https://snoyuhumzxmrbdzeacrc.supabase.co";
-// const supabaseKey = SUPABASE_KEY;
-// const supabase = createClient(supabaseUrl, supabaseKey);
-
-// export default { supabase, supabaseUrl };
-
 import { createClient } from "@supabase/supabase-js";
+// Load environment variables
 
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNub3l1aHVtenhtcmJkemVhY3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4Mzg0NzksImV4cCI6MjAzNTQxNDQ3OX0.koHX73Dj_D8N5Z11iF-TUw6okVTZ0jGbMrppAECXfTY";
-const supabaseUrl = "https://snoyuhumzxmrbdzeacrc.supabase.co";
-const supabase = createClient(supabaseUrl, SUPABASE_KEY);
+const supabaseKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
